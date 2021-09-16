872c31e6e3a0922d98a504531f3131261fbe6e2e
tash-2s <81064017+tash-2s@users.noreply.github.com>
2021-09-16 20:45:16 +0900

    Impl data

--- data/lib.rs
+++ data/lib.rs
@@ -4,71 +4,49 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod data {
+    use ink_prelude::{vec as std_vec, vec::Vec as StdVec};
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
     pub struct Data {
-        /// Stores a single `bool` value on the storage.
         value: bool,
+        allowlist: StdVec<AccountId>,
     }
 
     impl Data {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
         pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+            Self {
+                value: init_value,
+                allowlist: std_vec![Self::env().caller()],
+            }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
-        #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
         pub fn get(&self) -> bool {
             self.value
         }
-    }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// Imports `ink_lang` so we can use `#[ink::test]`.
-        use ink_lang as ink;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let data = Data::default();
-            assert_eq!(data.get(), false);
+        #[ink(message)]
+        pub fn get_allowlist(&self) -> StdVec<AccountId> {
+            self.allowlist.clone()
         }
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut data = Data::new(false);
-            assert_eq!(data.get(), false);
-            data.flip();
-            assert_eq!(data.get(), true);
+        #[ink(message)]
+        pub fn set(&mut self, new_value: bool) {
+            assert!(self.allowlist.contains(&self.env().caller()), "not allowed");
+            self.value = new_value;
+        }
+
+        #[ink(message)]
+        pub fn add_allowed_account(&mut self, new_account_id: AccountId) {
+            assert!(self.allowlist.contains(&self.env().caller()), "not allowed");
+            self.allowlist.push(new_account_id);
+        }
+
+        #[ink(message)]
+        pub fn remove_allowed_account(&mut self, account_id: AccountId) {
+            assert!(self.allowlist.contains(&self.env().caller()), "not allowed");
+            self.allowlist.retain(|a| a != &account_id);
         }
     }
 }
